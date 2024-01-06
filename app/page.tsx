7406a5f83609b76bd9ee685feb78300922cdae45
snigbar <snigbar@outlook.com>
2024-01-06 17:22:06 +0600

    banner added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
+import Banner from "./sections/Banner";
 
 export default function Home() {
   return (
-    <main className="flex 2xl:w-4/5 xl:w-10/12 lg:w-full  min-h-screen mx-auto flex-col items-center justify-between p-4">
+    <main className="flex 2xl:w-4/5 xl:w-10/12 lg:w-full  min-h-screen mx-auto flex-col items-center p-4">
       <Navbar />
+      <Banner></Banner>
     </main>
   );
 }
--- app/sections/Banner.tsx
+++ app/sections/Banner.tsx
@@ -1,5 +1,19 @@
+import Image from "next/image";
 import React from "react";
+import heroImage from "@/public/heroImg.png";
 
 export default function Banner() {
-  return <div>Banner</div>;
+  return (
+    <div className="bg-[#F99F1C] rounded-3xl w-full px-20 my-auto flex flex-col lg:flex-row justify-between items-center">
+      <div className="w-full flex flex-col gap-3 md:gap-4 lg:gap-6 my-4 lg:my-0">
+        <h1 className="lg:text-[40px] text-3xl text-center lg:text-left font-semibold text-white leading-[3rem] tracking-wider">
+          Deliver Food To Your Door Step |
+        </h1>
+        <p className="text-white text-lg lg:text-xl 2xl:text-2xl font-extralight text-center lg:text-left">
+          Authentic Food, Quick Service, Fast Delivery
+        </p>
+      </div>
+      <Image alt="hero image" src={heroImage} height={320}></Image>
+    </div>
+  );
 }
